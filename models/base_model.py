0d19b20b5fb77239f266dfcf4db39d150e1d9105
Sibongile <sibongile.nhlema@gmail.com>
2024-01-08 21:03:37 +0000

    LInked BaseModel to FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 
 import uuid
 from datetime import datetime
-
+from engine.file_storage import storage
 
 class BaseModel:
     ''' 
@@ -32,6 +32,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         '''
@@ -46,8 +47,11 @@ class BaseModel:
         '''
         Updates the public instance attribute
         'updated_at' with current datetime
+
+        Saves instances
         '''
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         '''
