551ad4c1abc17c36f8c148d68034ac48d7f9d5aa
Sibongile <sibongile.nhlema@gmail.com>
2024-01-09 11:09:25 +0000

    added console.py contents

--- console.py
+++ console.py
@@ -1,1 +1,43 @@
 #!/usr/bin/python3
+''' This module conatins the entry point of the command interpreter '''
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    '''
+    This is the entry point for the command intepreter
+
+    Features:
+        - quit and EOF to exit porgram
+        - help (updated and documented)
+        - custom prompt (hbnb)
+        - an empty line + ENTER has no effect
+
+    Does NOT execute when imported
+    '''
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        '''
+        Quit command to exit the program
+        '''
+        return True
+
+    def do_EOF(self, line):
+        '''
+        EOF command cleanly quits/exits the program
+        '''
+        return True
+
+    # To do: implement the custom help method
+
+    def emptyline(self):
+        '''
+        Emptyline command has no effect/does nothing.
+        '''
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
