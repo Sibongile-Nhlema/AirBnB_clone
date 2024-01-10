d5a908ab4c87bbd9289f9f9e7ef46f957e6b119a
Maddie <115709272+Maddily@users.noreply.github.com>
2024-01-10 15:36:24 +0200

    Test User class

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Tests the User class."""
+
+import unittest
+from models.user import User
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class TestUser(unittest.TestCase):
+    """Tests the User class."""
+
+    def setUp(self):
+        """Does nothing"""
+        pass
+
+    def tearDown(self):
+        """Resets storage."""
+
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_user_type(self):
+        """Tests the type of User class."""
+
+        user = User()
+        self.assertEqual(str(type(user)), "<class 'models.user.User'>")
+
+    def test_user_instance(self):
+        """Tests if a user object is an instance of User."""
+
+        user = User()
+        self.assertIsInstance(user, User)
+
+    def test_user_superclass(self):
+        """Tests if user is a subclass of BaseModel."""
+
+        user = User()
+        self.assertTrue(issubclass(type(user), BaseModel))
+
+    def test_user_attributes(self):
+        """Tests attributes of User class."""
+
+        attributes = {
+            'email': str,
+            'password': str,
+            'first_name': str,
+            'last_name': str
+        }
+        user = User()
+
+        for key, value in attributes.items():
+            self.assertTrue(hasattr(user, key))
+            self.assertEqual(type(getattr(user, key, None)), value)
+
+
+if __name__ == "__main__":
+    unittest.main()
