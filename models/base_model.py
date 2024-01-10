547d4a15a0efec27ec160f18b03b27a6a1086e80
Sibongile <sibongile.nhlema@gmail.com>
2024-01-10 11:59:50 +0000

    added files: base_model.py Amenity.py Review.py User.py File_storage.py __init__.py

---
+++ models/base_model.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+''' This is the module for the BaseModel Class '''
+
+import uuid
+import models as md
+from datetime import datetime
+
+
+class BaseModel:
+    '''
+    This class deinfes all common attributes for other classes
+
+    Public Instance Attributes:
+        id (str): The unique identifier for the instance.
+        created_at (datetime): The timestamp when the instance was created.
+        updated_at (datetime): The timestamp when the instance was last updated
+    '''
+    def __init__(self, *args, **kwargs):
+        '''
+        Constructor: Initializes an instance of the BaseModel class.
+
+        Args:
+            args (any) - Unused
+            kwargs (dict) - Key/Value arguments
+        '''
+        if kwargs:
+            for key, v in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            md.storage.new(self)
+
+    def __str__(self):
+        '''
+        Return:
+            A string representation of the instance in the format:
+            "[ClassName] (uuid) {key1: value1, ...}"
+        '''
+        return "[{}] ({}) {}".format((self.__class__.__name__),
+                                     self.id, self.__dict__)
+
+    def save(self):
+        '''
+        Updates the public instance attribute
+        'updated_at' with current datetime
+
+        Saves instances
+        '''
+        self.updated_at = datetime.now()
+        md.storage.save()
+
+    def to_dict(self):
+        '''
+        Return:
+            A dict containing all key/values of __dict__ of the instance.
+        '''
+        full_dictionary = self.__dict__.copy()
+        full_dictionary["created_at"] = self.created_at.isoformat()
+        full_dictionary["updated_at"] = self.updated_at.isoformat()
+        full_dictionary['__class__'] = self.__class__.__name__
+        return full_dictionary
