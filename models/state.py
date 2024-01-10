33af8b795ea18beddd58b089d5d87da5d4a9350b
Maddie <115709272+Maddily@users.noreply.github.com>
2024-01-10 14:42:47 +0200

    Create State class

---
+++ models/state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module contains the class State that defines a state.
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Defines a state.
+
+    Public Class Attributes:
+    - name (str): The name of a state
+    """
+
+    name = ""
