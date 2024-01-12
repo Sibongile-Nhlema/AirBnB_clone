68f6f8206c6cb8828a9aed5c10f50773ba5f9f1e
Maddie <115709272+Maddily@users.noreply.github.com>
2024-01-12 16:54:11 +0200

    Implement dot notation and dictionary input

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ This module defines the entry point of the command interpreter.
 
 import cmd
 import re
+import ast
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
@@ -180,6 +181,81 @@ class HBNBCommand(cmd.Cmd):
             str_representations = [f'"{str(obj)}"' for obj in objects.values()]
             print('[' + ', '.join(str_representations) + ']')
 
+    def do_update(self, line):
+        """
+        Updates an instance based on the class name and id
+        by adding or updating attribute.
+
+        Args:
+        - line (str): The command line
+        """
+
+        args = line.split()
+        class_name = args[0] if args else None
+        id = args[1] if len(args) > 1 else None
+        attr_name = args[2] if len(args) > 2 else None
+        attr_value = args[3] if len(args) > 3 else None
+
+        attr_name = remove_quotes(attr_name)
+        attr_value = remove_quotes(attr_value)
+
+        if attr_value and attr_value.isdigit():
+            attr_value = int(attr_value)
+        elif attr_value and is_float(attr_value):
+            attr_value = float(attr_value)
+
+        if not class_name:
+            print("** class name missing **")
+        else:
+            if class_name not in HBNBCommand.__classes:
+                print("** class doesn't exist **")
+            elif not id:
+                print("** instance id missing **")
+            else:
+                key = f"{class_name}.{id}"
+                objects = storage.all()
+
+                if key in objects:
+                    if ('{' in line):
+                        line_match = re.match(r"^.+(\{.+\})$", line)
+                        dictionary = ast.literal_eval(line_match.group(1))
+                        self.dictionary_update(class_name, id, dictionary)
+                    else:
+                        if not attr_name:
+                            print("** attribute name missing **")
+                        else:
+                            if not attr_value:
+                                print("** value missing **")
+                            else:
+                                obj = objects[key]
+                                setattr(obj, attr_name, attr_value)
+                                obj.save()
+                else:
+                    print("** no instance found **")
+
+    def do_count(self, class_name):
+        """
+        Retrieves the number of instances of a class.
+
+        Args:
+        - class_name (str): The name of a class
+        """
+
+        if class_name:
+            if class_name not in HBNBCommand.__classes:
+                print("** class doesn't exist **")
+            else:
+                count = 0
+                objects = storage.all()
+
+                for key in objects.keys():
+                    if key.split('.')[0] == class_name:
+                        count += 1
+
+                print(count)
+        else:
+            print("** class name missing **")
+
     def dot_notation_all(self, class_name):
         """
         Prints string representation of all instances,
@@ -275,23 +351,18 @@ class HBNBCommand(cmd.Cmd):
 
         print(count)
 
-    def do_update(self, line):
+    def dot_notation_update(
+            self, class_name, instance_id, attr_name, attr_value
+            ):
         """
-        Updates an instance based on the class name and id
-        by adding or updating attribute.
+        Updates an instance based on its id.
 
         Args:
-        - line (str): The command line
+        - class_name (str): The name of a class
+        - instance_id (str): The id of an object
+        - attr_name (str): The attribute to be added/updated
+        - attr_value: attr_name's value
         """
-
-        args = line.split()
-        class_name = args[0] if args else None
-        id = args[1] if len(args) > 1 else None
-        attr_name = args[2] if len(args) > 2 else None
-        attr_value = args[3] if len(args) > 3 else None
-
-        attr_name = remove_quotes(attr_name)
-        attr_value = remove_quotes(attr_value)
 
         if attr_value and attr_value.isdigit():
             attr_value = int(attr_value)
@@ -303,10 +374,10 @@ class HBNBCommand(cmd.Cmd):
         else:
             if class_name not in HBNBCommand.__classes:
                 print("** class doesn't exist **")
-            elif not id:
+            elif not instance_id:
                 print("** instance id missing **")
             else:
-                key = f"{class_name}.{id}"
+                key = f"{class_name}.{instance_id}"
                 objects = storage.all()
 
                 if key in objects:
@@ -319,6 +390,54 @@ class HBNBCommand(cmd.Cmd):
                             obj = objects[key]
                             setattr(obj, attr_name, attr_value)
                             obj.save()
+                else:
+                    print("** no instance found **")
+
+    def dictionary_update(self, class_name, instance_id, dictionary):
+        """
+        Updates attributes of an instance based on its id,
+        using data provided in a dictionary.
+
+        Args:
+        - class_name (str): The name of a class
+        - instance_id (str): The id of an instance
+        - dictionary (dict): A dictionary containining attributes and values
+        """
+
+        if not class_name:
+            print("** class name missing **")
+        else:
+            if class_name not in HBNBCommand.__classes:
+                print("** class doesn't exist **")
+            elif not instance_id:
+                print("** instance id missing **")
+            else:
+                key = f"{class_name}.{instance_id}"
+                objects = storage.all()
+
+                if key in objects:
+                    for attr_name, attr_value in dictionary.items():
+                        if (
+                                not isinstance(attr_value, int)
+                                and not isinstance(attr_value, float)
+                                and attr_value.isdigit()
+                            ):
+                            attr_value = int(attr_value)
+                        elif (
+                                not isinstance(attr_value, int)
+                                and not isinstance(attr_value, float)
+                                and is_float(attr_value)
+                                ):
+                            attr_value = float(attr_value)
+                        if not attr_name:
+                            print("** attribute name missing **")
+                        else:
+                            if not attr_value:
+                                print("** value missing **")
+                            else:
+                                obj = objects[key]
+                                setattr(obj, attr_name, attr_value)
+                                obj.save()
                 else:
                     print("** no instance found **")
 
@@ -340,21 +459,48 @@ class HBNBCommand(cmd.Cmd):
         - line (str): The command line
         """
 
-        match_all = re.match(r"^(.+)\.(.+)\((.*)\)$", line)
-        instance_id = match_all.group(3)
+        methods = ['all', 'count', 'show', 'destroy', 'update']
+
+        match_line = re.match(r"^(.+)\.(.+)\(([^,]+)?,?\s?(\{.+\}|[^,]+)?,?\s?(.+)?\)$", line)
+
+        if not match_line:
+            match_all = re.match(r"^\.all\(\)$", line)
+            if match_all:
+                self.do_all('')
+                return
+            else:
+                print("** class name missing **")
+                return
+
+        instance_id = match_line.group(3)
+        attr_name = match_line.group(4)
+        attr_value = match_line.group(5)
 
         instance_id = remove_quotes(instance_id)
+        attr_name = remove_quotes(attr_name)
+        attr_value = remove_quotes(attr_value)
 
-        if match_all:
-            line = match_all.group(1)
-            if match_all.group(2) == 'all':
+        if match_line:
+            line = match_line.group(1)
+            if match_line.group(2) == 'all':
                 self.dot_notation_all(line)
-            elif match_all.group(2) == 'count':
+            elif match_line.group(2) == 'count':
                 self.dot_notation_count(line)
-            elif match_all.group(2) == 'show':
+            elif match_line.group(2) == 'show':
                 self.dot_notation_show(line, instance_id)
-            elif match_all.group(2) == 'destroy':
+            elif match_line.group(2) == 'destroy':
                 self.dot_notation_destroy(line, instance_id)
+            elif match_line.group(2) == 'update':
+                if (
+                        attr_name and attr_name.startswith("{")
+                        and attr_name.endswith('}')
+                        ):
+                    dictionary = ast.literal_eval(attr_name)
+                    self.dictionary_update(line, instance_id, dictionary)
+                else:
+                    self.dot_notation_update(line, instance_id, attr_name, attr_value)
+            else:
+                print(f"*** Unknown syntax: {line}.{match_line.group(2)}()")
 
     def help_create(self):
         """Prints documentation for create command."""
@@ -380,6 +526,12 @@ class HBNBCommand(cmd.Cmd):
         print("Usage: all <class_name>")
         print("Usage: all")
         print("Print string representation of all instances.")
+
+    def help_count(self):
+        """Prints documentation for count command"""
+
+        print("Usage: count <class> or <class>.count()")
+        print("Retrieve the number of instances of a class.")
 
     def help_update(self):
         """Prints documentation for update command."""
