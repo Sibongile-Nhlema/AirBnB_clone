981235d4b8599cad052462026f44382fa99ce7b2
Maddie <115709272+Maddily@users.noreply.github.com>
2024-01-13 16:05:38 +0200

    Update importing storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 ''' This is the module for the BaseModel Class '''
 
 import uuid
-import models as md
+from models import storage
 from datetime import datetime
 
 
@@ -33,7 +33,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            md.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         '''
@@ -52,7 +52,7 @@ class BaseModel:
         Saves instances
         '''
         self.updated_at = datetime.now()
-        md.storage.save()
+        storage.save()
 
     def to_dict(self):
         '''
