dd399d52d32fe933edaa274cbdb38ffdf36d5489
Maddie <115709272+Maddily@users.noreply.github.com>
2024-01-13 18:42:31 +0200

    Add a blank line

--- tests/test_console.py
+++ tests/test_console.py
@@ -261,6 +261,7 @@ class TestHBNBCommand_create(unittest.TestCase):
             self.assertLess(0, len(output.getvalue().strip()))
             testKey = "Place.{}".format(output.getvalue().strip())
             self.assertIn(testKey, storage.all().keys())
+
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("create Review"))
             self.assertLess(0, len(output.getvalue().strip()))
