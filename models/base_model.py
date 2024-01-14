f20a2744cfe9a55a5341104cc40ee584b65f25c2
Maddie <115709272+Maddily@users.noreply.github.com>
2024-01-14 08:31:13 +0200

    Update storage variable importing

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 ''' This is the module for the BaseModel Class '''
 
 import uuid
-import models as md
+from models import storage
 from datetime import datetime
 
 
@@ -31,7 +31,6 @@ class BaseModel:
                             value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
-            from models import storage
 
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -54,8 +53,6 @@ class BaseModel:
 
         Saves instances
         '''
-
-        from models import storage
 
         self.updated_at = datetime.now()
         storage.save()
