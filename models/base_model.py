51923009f20812b815910b0be6e80755453af70d
Sibongile <sibongile.nhlema@gmail.com>
2024-01-14 10:23:05 +0000

    reverted the changes made to import storage

--- models/base_model.py
+++ models/base_model.py
@@ -30,12 +30,10 @@ class BaseModel:
                         value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
-            from models import storage
-
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            md.storage.new(self)
 
     def __str__(self):
         '''
@@ -53,11 +51,8 @@ class BaseModel:
 
         Saves instances
         '''
-
-        from models import storage
-
         self.updated_at = datetime.now()
-        storage.save()
+        md.storage.save()
 
     def to_dict(self):
         '''
